fb94050595ffe19541c8d8174478a2faf6195a36
JaneMoroz <moroz.jane@yahoo.com>
2022-05-17 15:58:31 +0300

    Added Error Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SingleItem from "./pages/SingleItem";
+import Error from "./pages/Error";
 import SharedLayout from "./SharedLayout";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<SharedLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="menu/:id" element={<SingleItem />} />
+        <Route path="*" element={<Error />} />
       </Route>
     </Routes>
   );
---
+++ client/src/pages/Error.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <div className="error">
+      <p className="status">404!</p>
+      <h1>Page doesn't exist</h1>
+      <Link to="/" type="button" className="btn btn--secondary">
+        Home
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
